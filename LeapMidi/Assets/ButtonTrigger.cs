c47b6b22f6bafa639403f3c4d29c76521a86884e
emilienbai <emilien.bai@insa-lyon.fr>
2016-11-30 15:27:39 +0100

    Add basic project to work with

---
+++ LeapMidi/Assets/ButtonTrigger.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Sanford.Multimedia.Midi;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    private Collider initialObject;
+    private int id;
+    private bool oneIn;
+    public float force;
+    public byte midiID;
+
+    public static OutputDevice outputDevice;
+
+    // Use this for initialization
+    void Start () {
+        oneIn = false;
+
+        id = MidiScript.getMidiPortID();
+        if (outputDevice == null)
+        {
+            outputDevice = new OutputDevice(id);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!oneIn)
+        {
+            ChannelMessage message = new ChannelMessage(ChannelCommand.NoteOn, 0, midiID, 127);
+            outputDevice.Send(message);
+            initialObject = other;
+            oneIn = true;
+        }
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if(initialObject && other == initialObject)
+        {
+            initialObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -force), ForceMode.Acceleration);
+            initialObject.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == initialObject)
+        {
+            initialObject.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+            initialObject.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+            oneIn = false;
+            initialObject = null;
+            ChannelMessage message = new ChannelMessage(ChannelCommand.NoteOff, 0, midiID, 127);
+            outputDevice.Send(message);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        outputDevice.Dispose();
+    }
+}
---
+++ LeapMidi/Assets/MidiScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using TobiasErichsen.teVirtualMIDI;
+using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+public class MidiScript : MonoBehaviour
+{
+
+    public TeVirtualMIDI midiPort;
+    Thread midiThread;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        midiPort = startMidiPort();
+        midiThread = new Thread(new ThreadStart(SendReceiveMIDI));
+        midiThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SendReceiveMIDI();
+    }
+
+    void OnApplicationQuit()
+    {
+        midiThread.Abort();
+        midiPort.shutdown();
+        Debug.Log("Application ending after " + Time.time + " seconds");
+    }
+
+    private TeVirtualMIDI startMidiPort()
+    {
+        Debug.Log("Up and running");
+        //LaunchMidi();
+        Console.WriteLine("teVirtualMIDI C# loopback sample");
+        Console.WriteLine("using dll-version:    " + TeVirtualMIDI.versionString);
+        Console.WriteLine("using driver-version: " + TeVirtualMIDI.driverVersionString);
+
+        TeVirtualMIDI.logging(TeVirtualMIDI.TE_VM_LOGGING_MISC | TeVirtualMIDI.TE_VM_LOGGING_RX | TeVirtualMIDI.TE_VM_LOGGING_TX);
+
+        Guid manufacturer = new Guid("aa4e075f-3504-4aab-9b06-9a4104a91cf0");
+        Guid product = new Guid("bb4e075f-3504-4aab-9b06-9a4104a91cf0");
+
+        TeVirtualMIDI port = new TeVirtualMIDI("LeapMidi1", 65535, TeVirtualMIDI.TE_VM_FLAGS_PARSE_RX, ref manufacturer, ref product);
+        Debug.Log("Midi Port started !");
+        return port;
+    }
+
+
+    public void SendReceiveMIDI()
+    {
+        byte[] command;
+        try
+        {
+            while (true)
+            {
+                command = midiPort.getCommand();
+                midiPort.sendCommand(command);
+                //Debug.Log("command: " + byteArrayToString(command));
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Fail while sending / retrieving message: " + ex.Message);
+        }
+    }
+
+    public static string byteArrayToString(byte[] ba)
+    {
+        string hex = BitConverter.ToString(ba);
+        return hex.Replace("-", ":");
+
+    }
+
+    [DllImport("winmm.dll")]
+    protected static extern int midiOutGetNumDevs();
+
+    public static int getMidiPortID()
+    {
+        return midiOutGetNumDevs() - 1;
+    }
+
+}
