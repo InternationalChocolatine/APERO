e8d6b2c9c1000a443fe75bea2fc74fe264643a9c
maxime-christ <maxime.christ@insa-lyon.fr>
2016-12-01 17:57:52 +0100

    forgot to add right files to previous commit

---
+++ LeapMidi/Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Sanford.Multimedia.Midi;
+using System.Threading;
+using System;
+
+/*
+ * Small collider script sending midi message on virtual midi port
+ * port is set in id variable
+ * port = 0 is the basic microsoft synth, it can be usefull for testing purposes
+ * WARNING : to use this script in unity, you have to use the 3 dll provided in the lib folder
+ * Theses dll have to be placed in the asset folder of the unity project
+ * */
+
+public class ColliderScript : MonoBehaviour {
+    int id = 1;
+    private static int counter = 60;
+    private int note;
+    int collisionNumber = 0;
+    public static OutputDevice outputDevice;
+	// Use this for initialization
+	void Start () {
+        note = counter;
+        counter += 1;        Debug.Log("Device started ! : " + note);
+        id = MidiScript.getMidiPortID();
+        if (outputDevice == null)
+        {
+            outputDevice = new OutputDevice(id);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(collisionNumber == 0)
+        {
+            ChannelMessage message = new ChannelMessage(ChannelCommand.NoteOn, 0, note, 127);
+        
+            outputDevice.Send(message);
+        }
+        collisionNumber++;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        collisionNumber--;
+        if(collisionNumber == 0)
+        {
+            ChannelMessage message = new ChannelMessage(ChannelCommand.NoteOff, 0, note, 127);
+            outputDevice.Send(message);
+        }
+        
+    }
+
+    void OnApplicationQuit(){
+        outputDevice.Dispose();
+    }
+}
---
+++ LeapMidi/Assets/Scripts/PotentiometerBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Leap;
+
+public class PotentiometerBehaviour : MonoBehaviour {
+    private bool isRightHand;
+    private bool grabbed;
+    private Controller controller;
+    private float refAngle;
+
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+        grabbed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (grabbed == true)
+        {
+            float currentAngle;
+            System.Collections.Generic.List<Hand> handList = controller.Frame().Hands;
+            for(int i = 0; i < handList.Count; i++)
+            {
+                if(handList[i].IsLeft != isRightHand)
+                {
+                    currentAngle = -handList[i].Direction.Roll + (handList[i].Direction.Roll / Mathf.Abs(handList[i].Direction.Roll)) * Mathf.PI;
+                    Debug.Log("Updated! new value is " + currentAngle);
+                    transform.Rotate(Vector3.up, 20 * (currentAngle - refAngle));
+                    refAngle = currentAngle;
+                }
+            }
+        }
+    }
+
+    public void Pinch(bool rightHand)
+    {
+        grabbed = true;
+        isRightHand = rightHand;
+
+        System.Collections.Generic.List<Hand> handList = controller.Frame().Hands;
+        for(int i = 0; i < handList.Count; i++)
+        {
+            if(handList[i].IsLeft != isRightHand)
+            {
+                refAngle = -handList[i].Direction.Roll + (handList[i].Direction.Roll / Mathf.Abs(handList[i].Direction.Roll)) * Mathf.PI;
+                Debug.Log("Initial value is " + refAngle);
+            }
+        }
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.red;
+    }
+
+    public void Unpinch()
+    {
+        grabbed = false;
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.white;
+    }
+}
